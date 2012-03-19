5f818521b726915e6382b772f9cfc5bf3974258d
Eric Rasmussen <ericrasmussen1@gmail.com>
2012-03-19 17:24:30 -0500

    Work on Campus data and KML

--- src/UNL/OpenMap/MarkerList/KML.php
+++ src/UNL/OpenMap/MarkerList/KML.php
@@ -17,27 +17,48 @@ class UNL_OpenMap_MarkerList_KML extends ArrayIterator implements UNL_OpenMap_Ma
         $info = array();
 
         foreach ($this->xml->Document->Placemark as $placemark) {
-            if (isset($placemark->Point)) {
-                $geo = $placemark->Point->coordinates;
-                $geo = explode(',', trim((string)$geo));
-                $info[] = array('lat'=>(float)$geo[1], 'lng'=>(float)$geo[0], 'title'=> $this->title.": ".(string)$placemark->name, 'code'=>count($info));
-            } else if (isset($placemark->Polygon)) {
-                $geo = $placemark->Polygon->outerBoundaryIs->LinearRing->coordinates;
-                $coords = preg_split("/[\s]+/", trim((string)$geo));echo '<pre>';var_dump($coords);exit;
-                $info[] = array('lat'=>(float)$geo[1], 'lng'=>(float)$geo[0], 'title'=> $this->title.": ".(string)$placemark->name, 'code'=>count($info));
+            if (isset($placemark->Point) || isset($placemark->Polygon)) {
+                if (isset($placemark->Point)) {
+                    $geo = $placemark->Point->coordinates;
+                    $geo = explode(',', trim((string)$geo));
+                }
+                if (isset($placemark->Polygon)) {
+                    $poly = $placemark->Polygon->outerBoundaryIs->LinearRing->coordinates;
+                    preg_match_all("/[\s]+(?P<lon>\S+),(?P<lat>\S+),0.0*[\s]+/", $poly, $matches);
+                    foreach ($matches[0] as $key => $match) {
+                        $coords[$key]['lat'] = $matches['lat'][$key];
+                        $coords[$key]['lon'] = $matches['lon'][$key];
+                    }
+                }
             } else {
                 throw new Exception('Do not know how to work with supplied KML file',404);
             }
+
+            $info[] = array(
+                        'lat' => $geo[1],
+                        'lng' => $geo[0],
+                        'coords' => (isset($coords) ? $coords : array()),
+                        'title' => $placemark->name,
+                        'code' => count($info));
         }
+
         parent::__construct($info);
     }
 
     function current()
     {
-        $point = parent::current();
-        $position = new UNL_OpenMap_LatLng(array('lat'=>$point['lat'], 'lng'=>$point['lng']));
         $code = $this->key();
 
-        return new $this->markerClass(array('position'=>$position, 'title'=>$point['title'], 'code'=>$code));
+        $data = parent::current();
+
+        $position = new UNL_OpenMap_LatLng(array('lat' => $data['lat'], 'lng' => $data['lng']));
+
+        $polygon = new UNL_OpenMap_Polygon(array('coords' => $data['coords']));
+
+        return new $this->markerClass(array(
+                                        'position' => $position,
+                                        'polygon' => $polygon,
+                                        'title' => $data['title'],
+                                        'code' => $code));
     }
 }
---
+++ www/templates/json/UNL/OpenMap/Marker/Campus.tpl.php
@@ -0,0 +1,22 @@
+{
+    "code":"<?php echo $context->code; ?>",
+    "name":"<?php echo $context->title; ?>",
+    "position":{
+        "point":
+            {
+            "latitude":"<?php echo $context->position->lat; ?>",
+            "longitude":"<?php echo $context->position->lng; ?>"
+            },
+        "polygon":
+            [
+            <?php $last_key = end(array_keys($context->polygon->coords));
+            foreach ($context->polygon->coords as $key => $coords): ?>
+                {
+                    "latitude":"<?php echo $coords->lat; ?>",
+                    "longitude":"<?php echo $coords->lng; ?>"
+                }<?php echo ($key == $last_key ? '' : ',');?>
+
+            <?php endforeach; ?>
+            ]
+    }
+}
